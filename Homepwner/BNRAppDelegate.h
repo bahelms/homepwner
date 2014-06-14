617c55c99c684b45eab4cfdb9a01c3e98fd8117e
bahelms <jimbonkgit@gmail.com>
2014-06-14 11:40:34 -0400

    initial app setup

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Barrett Helms on 6/14/14.
+//  Copyright (c) 2014 Helms Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
