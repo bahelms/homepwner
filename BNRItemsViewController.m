42b9738529319c5505a8227ad45e971183777053
bahelms <jimbonkgit@gmail.com>
2014-06-20 09:55:52 -0400

    adds table view cell stuff

--- BNRItemsViewController.m
+++ BNRItemsViewController.m
@@ -25,4 +25,31 @@
 
 - (instancetype)initWithStyle:(UITableViewStyle)style { return [self init]; }
 
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+}
+
+
+#pragma mark - DataSource
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Get a new or recycled cell
+    UITableViewCell *cell =
+        [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                        forIndexPath:indexPath];
+    cell.textLabel.text = [[[BNRItemStore sharedStore] allItems][indexPath.row] description];
+    return cell;
+}
+
 @end
